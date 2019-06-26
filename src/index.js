48e0d86bc8f070b89ef24f1b963a113da729eaa4
RedAuthorium <42084635+RedAuthorium@users.noreply.github.com>
2019-06-26 14:05:59 +0700

    jsx, component, properties

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
+// function greet(nama) {
+//     return "halo " + nama;
+// }
+
+// const name = "rifky";
+// const element = <h1> {greet(name)} </h1>;
+
+// ReactDOM.render(element, document.getElementById('greeting'));
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
